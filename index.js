49f82e987965a56d7df3932da0ff11e33e8657fa
kiricon <kiricon@live.com>
2017-02-09 18:51:14 -0700

    Built basic component parent class and render class

--- index.js
+++ index.js
@@ -8,12 +8,29 @@ class Component {
      */
     constructor() {
         if (new.target === Abstract) {
-        throw new TypeError('Cannot construct Abstract instances directly');
+            throw new TypeError('Cannot construct Abstract instances directly');
         }
+
+        // Convert our string in to an object we can mess with.
+        let xmlString = this.render();
+        let parser = new DOMParser();
+        let doc = parser.parseFromString(xmlString, 'text/xml');
+        this.el = doc.firstChild;
     }
+}
+
+/**
+ * Function to render a component
+ * @param {Component} component - Component to render
+ * @param {Element} parent - parent element to render compponent in
+ * @return {Void}
+ */
+function render(component, parent) {
+    parent.appendChild(component.el);
 }
 
 
 module.exports = {
     Component: Component,
+    Render: render,
 };
